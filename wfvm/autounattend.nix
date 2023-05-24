bc24fd6a2bb23de8f3dcef36a54fea5cb92751e8
Astro <astro@spaceboyz.net>
2023-05-24 17:58:57 +0200

    autounattend: remove more optional bypasses

--- wfvm/autounattend.nix
+++ wfvm/autounattend.nix
@@ -151,18 +151,6 @@ let
               <Order>1</Order>
               <Path>reg add HKLM\System\Setup\LabConfig /v BypassTPMCheck /t reg_dword /d 0x00000001 /f</Path>
             </RunSynchronousCommand>
-            <RunSynchronousCommand wcm:action="add">
-              <Order>2</Order>
-              <Path>reg add HKLM\System\Setup\LabConfig /v BypassRAMCheck /t reg_dword /d 0x00000001 /f</Path>
-            </RunSynchronousCommand>
-            <RunSynchronousCommand wcm:action="add">
-              <Order>3</Order>
-              <Path>reg add HKLM\System\Setup\LabConfig /v BypassCPUCheck /t reg_dword /d 0x00000001 /f</Path>
-            </RunSynchronousCommand>
-            <RunSynchronousCommand wcm:action="add">
-              <Order>4</Order>
-              <Path>reg add HKLM\System\Setup\LabConfig /v BypassStorageCheck /t reg_dword /d 0x00000001 /f</Path>
-            </RunSynchronousCommand>
           </RunSynchronous>
 
           <DiskConfiguration>

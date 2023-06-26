ec1c08956bba745e2d240ac039a5f93e1b95e81a
Astro <astro@spaceboyz.net>
2023-06-26 15:48:18 +0200

    default.nix: remove superfluous parentheses

--- wfvm/default.nix
+++ wfvm/default.nix
@@ -2,6 +2,6 @@
 
 {
   makeWindowsImage = attrs: import ./win.nix ({ inherit pkgs; } // attrs);
-  layers = (import ./layers { inherit pkgs; });
-  utils = (import ./utils.nix { inherit pkgs; });
+  layers = import ./layers { inherit pkgs; };
+  utils = import ./utils.nix { inherit pkgs; };
 }
